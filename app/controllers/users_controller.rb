6eab00012d089dc159ea6c90240c2dc3e58f3655
sugiuraryouga <61176802+sugiuraryouga@users.noreply.github.com>
2020-03-01 17:45:28 +0900

    add_serach

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
- def edit
- end
- def update
-  if current_user.update(user_params)
-    redirect_to root_path
-  else
-    render :edit
+  def index
+    return nil if params[:keyword] == ""
+    @users = User.where(['name LIKE ?', "%#{params[:keyword]}%"] ).where.not(id: current_user.id).limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
- end
+  
+  def edit
+  end
+ 
+  def update
+   if current_user.update(user_params)
+    redirect_to root_path
+   else
+    render :edit
+   end
+  end
 end
 
 private
