6987b002ad198bd62d4730f6c34eb89b5ac1eb27
sugiuraryouga <61176802+sugiuraryouga@users.noreply.github.com>
2020-03-06 16:38:26 +0900

    hoge

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -81,7 +81,6 @@ $('#new_message').on('submit', function(e){
     var reloadMessages = function() {
       //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
       var last_message_id = $('.chat-main__messages__message:last').data("message-id");
-      console.log(last_message_id)
       $.ajax({
         //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
         url: "api/messages",
